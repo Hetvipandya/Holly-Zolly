e490ac7eaefb192ed3451981a4a9aeb324b58bfc
Hetvi_Pandya <pandyahetvi121@gmail.com>
2026-04-23 11:38:05 +0530

    Add Sanity backend and lib directory

---
+++ src/lib/sanity.js
@@ -0,0 +1,19 @@
+import { createClient } from "@sanity/client";
+
+// 🔥 Sanity Client
+export const client = createClient({
+  projectId: "5o6gm3q4",
+  dataset: "categories",
+  apiVersion: "2024-01-01",
+  useCdn: false, 
+  perspective: "published",
+});
+
+// 🔥 Image URL Builder (optional but useful)
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(client);
+
+export function urlFor(source) {
+  return builder.image(source);
+}
